9359c7ba6fb6854777c9bec038d7ec1a84c54bc6
alejandro861 <gmalejan861@gmail.com>
2019-12-04 22:59:58 -0600

    Commit#2
    Se envia un mensaje de nueva visualización cada vez que se presiona la tecla "Enter"

--- dadoCSharp3/Program.cs
+++ dadoCSharp3/Program.cs
@@ -6,7 +6,22 @@ namespace dadoCSharp3
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo tecla;
             Console.WriteLine("Hello World!");
+
+            do
+            {
+
+                tecla = Console.ReadKey(true);
+                if (tecla.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Nueva visualización...");
+                }
+                else if(tecla.Key != ConsoleKey.Enter && tecla.Key != ConsoleKey.Escape)
+                {
+                    Console.WriteLine("La tecla no cumple ningun comando...");
+                }
+            } while (tecla.Key != ConsoleKey.Escape);
         }
     }
 }
